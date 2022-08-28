196b2f09e2d43cf7cc70ef631a3f34a04fc070ce
Doğan Aker <dogan234@gmail.com>
2022-08-28 19:36:34 +0300

    .filter and .includes methods

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import "./App.css";
 
 class App extends Component {
   constructor() {
-    super()
+    super();
 
     this.state = {
       monsters: [],
@@ -12,35 +12,54 @@ class App extends Component {
   }
 
   componentDidMount() {
-    fetch('https://jsonplaceholder.typicode.com/users')
+    fetch("https://jsonplaceholder.typicode.com/users")
       // Convert response into json, so it can be used
       .then((response) => response.json())
-      // After resolve of above promise we will set 
+      // After resolve of above promise we will set
       // the users to our initially empty state
       .then((users) =>
         this.setState(
           () => {
-            return { monsters: users }
+            return { monsters: users };
           },
           () => {
-            console.log(this.state)
+            console.log(this.state);
           }
         )
-      )
+      );
   }
 
   render() {
     return (
       <div className="App">
+        <input
+          className="search-box"
+          type="search"
+          placeholder="Search Monsters"
+          onChange={(event) => {
+            const eventString = event.target.value.toLocaleLowerCase()
+            const newMonsters = this.state.monsters.filter((monster) => {
+              return monster.name.toLocaleLowerCase().includes(eventString)
+            })
+            this.setState(
+              () => {
+                return { monsters: newMonsters }
+              },
+              () => {
+                console.log(this.state)
+              }
+            )
+          }}
+        />
         {this.state.monsters.map((monster) => {
           return (
             <div key={monster.id}>
               <h1>{monster.name}</h1>
             </div>
-          )
+          );
         })}
       </div>
-    )
+    );
   }
 }
 
