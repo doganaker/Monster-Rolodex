05fa5c653476efb014148c0457e6fe9fdeb20891
Doğan Aker <dogan234@gmail.com>
2022-08-28 13:24:25 +0300

    mapping

--- src/App.js
+++ src/App.js
@@ -7,39 +7,28 @@ class App extends Component {
     super()
 
     this.state = {
-      name: { firstName: 'Yihua', lastName: 'Zhang' },
-      company: 'ZTM'
+      monsters: [
+        {
+          name: 'Linda',
+        },
+        {
+          name: 'Frank',
+        },
+        {
+          name: 'Jacky',
+        },
+        {
+          name: 'Andrei',
+        },
+      ]
     }
   }
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Hello, {this.state.name.firstName} {this.state.name.lastName}
-          </p>
-          <p>
-            Welcome to {this.state.company}
-          </p>
-          <button onClick={() => {
-            // Shallow Merge
-            // this.setState({
-            //   name: { firstName: 'Doğan', lastName: 'Aker' }
-            // })
-
-            // Secondary Callback
-            // Ideal and Optimal way of writing code
-            this.setState(() => {
-              return { name: { firstName: 'Doğan', lastName: 'Aker' } }
-            }, () => {
-              console.log('Secondary Callback State:', this.state)
-              // this one returns updated state, runs after below console.log
-            })
-            console.log('Original State', this.state)
-            // this one returns original state, runs before above console.log
-          }}>Change Name</button>
-        </header>
+        {this.state.monsters.map((monster) => {
+          return <h1>{monster.name}</h1>
+        })}
       </div>
     );
   }
